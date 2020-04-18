a7f66cce267449e380fda1e249225042388e8a5f
Victor Arnaud <victorhad@gmail.com>
2020-04-18 19:58:59 -0300

    fix eslint

--- {{cookiecutter.package_name}}/example/src/App.js
+++ {{cookiecutter.package_name}}/example/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Container } from 'react-bootstrap';
-import { Example } from '{{cookiecutter.package_name}}';
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import { Example } from "{{cookiecutter.package_name}}";
 
 class App extends Component {
   render() {
--- {{cookiecutter.package_name}}/src/index.js
+++ {{cookiecutter.package_name}}/src/index.js
@@ -1,3 +1,3 @@
-import React from 'react';
+import React from "react";
 
 export const Example = props => <h1>{props.children}</h1>;
